cdf53bea5962885303d6aa6519a763602a5f1c0c
Dare <darko.bodnaruk@gmail.com>
2014-02-26 18:04:44 +0100

    first

---
+++ prweb.py
@@ -0,0 +1,26 @@
+#!/home/dare/.virtualenvs/feedfilter/bin/python
+
+import jinja2
+import feedparser
+import cgi
+import cgitb
+cgitb.enable(display=1, logdir="/home/dare/cgi/")
+
+form = cgi.FieldStorage()
+if 'visibility' in form:
+	visibility = int(form['visibility'].value)
+else:
+	visibility = 49900
+z = feedparser.parse('http://www.prweb.com/feeds/zemanta.xml')
+items = [item for item in z['items'] if 'visibility' in item and int(item['visibility']) > visibility]
+#items = map(lambda item: item['summary'] = cgi.escape(item['summary']), items)
+for item in items:
+	item['summary'] = cgi.escape(item['summary'])
+	item['published'] = item['published']
+env = jinja2.Environment(loader=jinja2.FileSystemLoader("."))
+
+#print items
+#print
+print "Content-Type: text/xml"
+print
+print env.get_template("feedtemplate.xml").render(items=items).encode('utf-8')
